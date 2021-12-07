e26f37174e0d39a80f8d8497a4062c3e755af92d
Wataru SAKAMOTO <watskmt@gmail.com>
2021-12-07 09:34:28 +0900

    initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include <conio.h>
+
+void main(void)
+{
+	char key = 0;
+	int i, j;
+	int state[10][10] = {
+							{0,1,0,0,0, 0,0,0,0,0},
+							{0,0,1,0,0, 0,0,0,0,0},
+							{0,0,0,1,0, 0,0,0,0,0},
+							{0,0,0,0,1, 0,0,0,0,0},
+							{0,0,0,0,0, 1,0,0,0,0},
+
+							{0,0,0,0,0, 0,1,0,0,0},
+							{0,0,0,0,0, 0,0,1,0,0},
+							{0,0,0,0,0, 0,0,0,1,0},
+							{0,0,0,0,0, 0,0,0,0,1},
+							{0,0,0,0,0, 0,0,0,0,0}
+	};
+	char ch;
+
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j<10;j++)
+		{
+			switch (state[j][i])
+			{
+			case 0:
+				ch = ' ';
+				break;
+			case -1:
+				ch = 'X';
+				break;
+			case 1:
+				ch = 'O';
+				break;
+			default:
+				ch = '@';
+				break;
+			}
+			printf("%c", ch);
+		}
+		printf("\n");
+	}
+
+	
+	while (key != ' ')// スペースで終了
+	{
+			key = getch();
+			// if (key == 'w')printf("↑");
+			// if (key == 'z')printf("↓");
+			// if (key == 'a')printf("←");
+			// if (key == 's')printf("→");
+	}
+}
